32808358d573560c26902ec275e4981fc4200737
qiugang <849888900@qq.com>
2018-09-19 11:33:16 +0800

    create

---
+++ src/main/java/mavenproject/crane_zipkin_service/App.java
@@ -0,0 +1,13 @@
+package mavenproject.crane_zipkin_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
